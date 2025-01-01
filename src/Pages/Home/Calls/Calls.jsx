e8d80cdfea8f49a39c779e9189f1473f2bcb9b21
BD-Arfat <arfat1islam5162@gmail.com>
2025-01-01 22:32:50 +0600

    complite calls section

---
+++ src/Pages/Home/Calls/Calls.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Calls = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-20 bg-gray-100 py-20">
+        <div className="bg-black text-white text-center py-20 px-20 md:w-[900px] mx-auto rounded-lg shadow-lg">
+          <p className="text-3xl font-semibold">Call Us: +88 0192345678910</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calls;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
 import Abouts from "../Abouts/Abouts";
 import Menu from "../Menu/Menu";
+import Calls from "../Calls/Calls";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Category></Category>
       <Abouts></Abouts>
       <Menu></Menu>
+      <Calls></Calls>
     </div>
   );
 };
